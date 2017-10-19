d43f5ea3edb5e4174702728ae8960421145d1bbb
up201503447 <up201503447@fe.up.pt>
2017-10-19 11:27:35 +0100

    added tp3 files

---
+++ Exercícios/TP3/Ex2/sum2.php
@@ -0,0 +1,6 @@
+<?php  
+
+echo $_GET['num1'] . ' + ' . $_GET['num2'] . ' = ' . ($_GET['num1'] + $_GET['num2']);
+
+echo '<br> <button> <a href="form2.html" style="color:inherit; text-decoration:none;">Go back</a> </button> ';
+?>
