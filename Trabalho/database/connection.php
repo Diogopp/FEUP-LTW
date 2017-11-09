16420d7ca9be877ffd4ed06c19310ee91c0da924
eduardamsc <mariaeduardascunha97@gmail.com>
2017-11-09 11:29:49 +0000

    Changed around: removed files and added new ones for implementation

--- Trabalho/database/connection.php
+++ Trabalho/database/connection.php
@@ -1,6 +1,6 @@
 <?php
   try {
-     $dbh = new PDO('sqlite:blog.db');
+     $dbh = new PDO('sqlite:database/todue.db');
      $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $e) {
--- Trabalho/index.php
+++ Trabalho/index.php
@@ -1,17 +1,12 @@
-<?php
+  <?php
   session_start();
 
   include_once('database/connection.php');
-  include_once('database/post.php');
   include_once('templates/header.php');
 
-  try {
-    $posts = getAllPosts($dbh);
-  } catch (PDOException $e) {
-    die($e->getMessage());
-  }
-
+  
 unset($_SESSION['user']);
+//$_SESSION['user'] = 1;
 
 ?>
 
