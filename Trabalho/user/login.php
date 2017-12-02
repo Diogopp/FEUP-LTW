d14cc0446a21adec3577146b9feba96a9d9d59fc
up201503447 <up201503447@fe.up.pt>
2017-12-02 21:05:34 +0000

    merge

--- Trabalho/user/login.php
+++ Trabalho/user/login.php
@@ -1,14 +1,19 @@
 <?php
-  include('../database/connection.php');
+  session_start();
+  include(__DIR__ . '/../database/connection.php');
  try{
       $stmt = $dbh->prepare('SELECT idUser FROM User WHERE name = ? AND password = ?');
       $stmt->execute(array($_POST['userLog'], $_POST['passLog']));
-      if ($row = $stmt->fetch() != NULL)
-        $_SESSION['currentUser']= $row['idUser'];
+      if ( ($row = $stmt->fetch()) != NULL){
+          $_SESSION['currentUser']= $row['idUser'];
+          header("Location: ../index.php");
+      }
       else
         header("Location: ../index.php?failed");
+
     }
     catch (Exception $e) {
       echo 'Caught exception: ',  $e->getMessage(), "\n";
     }
+
 ?>
