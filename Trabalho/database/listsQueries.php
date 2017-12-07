b874ad6102a0c753e9529715d86eaeb31d8341b6
Diogopp <diogopeixotopereira@gmail.com>
2017-12-07 11:06:21 +0000

    new querys

--- Trabalho/database/listsQueries.php
+++ Trabalho/database/listsQueries.php
@@ -1,6 +1,6 @@
 <?php
 
-function getAllElements($dbh) {
+  function getAllElements($dbh) {
     try{
     $stmt = $dbh->prepare('SELECT * FROM ELEMENT WHERE idUser = ?');
     $stmt->execute(array($_SESSION['currentUser']));
@@ -12,20 +12,14 @@ function getAllElements($dbh) {
             <th align="left">Done</th>
             </tr>';
     while($row = $stmt->fetch()){
-      $deadline = date('d/m/Y', strtotime( $row['deadLine']));
       echo '
           <tr>
             <td>'. $row['tasks'].'</td>
-            <td>'. $deadline.'</td>';
+            <td>'. $row['deadLine'].'</td>';
       if ($row['done'])
         echo ' <td> <input type="checkbox" name="checkbox" disabled="disabled" checked> </td>';
-      else{
+      else
         echo ' <td> <input type="checkbox" name="checkbox" </td>';
-        echo'
-         <td>
-          <button id="rem_'.$row['idElement'].'" type="button" onclick="removeElementFromList('.$row['idElement'].')" > <i class="fa fa-close"></i></button>
-        </td>';
-      }
       echo' </tr>';
     }
     echo '</table>';
@@ -35,57 +29,126 @@ function getAllElements($dbh) {
   }
 }
 
-function getElementsByCategory($dbh, $category) {
-  try{
-  $stmt = $dbh->prepare('SELECT * FROM ELEMENT WHERE idCategory = ?');
-  $stmt->execute(array($category));
-  return $stmt->fetchAll();
+  function getElementsByCategory($dbh, $category) {
+    $stmt = $dbh->prepare('SELECT * FROM ELEMENT WHERE idCategory = ?');
+    $stmt->execute(array($category));
+    return $stmt->fetchAll();
   }
-  catch (Exception $e) {
-   echo 'Caught exception: ',  $e->getMessage(), "\n";
-  }
-}
 
-function getMaxElementID($dbh){
-  try{
-    $stmt = $dbh->prepare('SELECT idElement FROM Element ORDER BY idElement DESC LIMIT 1');
-    $stmt->execute();
-    if( ($row = $stmt->fetch()) == null)
-      $row['idElement'] = 0;
-    return $row['idElement'];
+  function updatePost($dbh, $id, $title, $introduction, $fulltext) {
+    $stmt = $dbh->prepare('UPDATE post SET title = ?, introduction = ?,  fulltext = ? WHERE id = ?');
+    $stmt->execute(array($title, $introduction, $fulltext, $id));
   }
-  catch (Exception $e) {
+
+
+  function getUserName($dbh) {
+    try{
+      $stmt = $dbh->prepare('SELECT name FROM USER WHERE idUser = ?');
+      $stmt->execute(array($_SESSION['currentUser']));
+
+      $row = $stmt->fetch();
+      echo 'Name: '. $row['name'];
+    }
+
+   catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
   }
 }
 
-function getMaxCategoryID($dbh){
-  try{
-    $stmt = $dbh->prepare('SELECT idCategory FROM CATEGORY ORDER BY idCategory DESC LIMIT 1');
-    $stmt->execute();
-    if( ($row = $stmt->fetch()) == null)
-      $row['idCategory'] = 0;
-    return $row['idCategory'];
-  }
-  catch (Exception $e) {
+
+
+
+////////////
+
+
+
+  function sortAphabetic($dbh) {
+    try{
+      $stmt = $dbh->prepare('SELECT * FROM ELEMENT WHERE idUser = ? Order by tasks');
+      $stmt->execute(array($_SESSION['currentUser']));
+      echo '
+            <table style="width:100%">
+                <tr>
+                  <th align="left">Task</th>
+                  <th align="left">Deadline</th>
+                  <th align="left">Done</th>
+                  </tr>';
+          while($row = $stmt->fetch()){
+            echo '
+                <tr>
+                  <td>'. $row['tasks'].'</td>
+                  <td>'. $row['deadLine'].'</td>';
+            if ($row['done'])
+              echo ' <td> <input type="checkbox" name="checkbox" disabled="disabled" checked> </td>';
+            else
+              echo ' <td> <input type="checkbox" name="checkbox" </td>';
+            echo' </tr>';
+            }
+      echo '</table>';
+    }
+
+   catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
   }
 }
 
-function getUserName($dbh) {
-  try{
-    $stmt = $dbh->prepare('SELECT name FROM USER WHERE idUser = ?');
-    $stmt->execute(array($_SESSION['currentUser']));
+function sortByCategory($dbh, $categor) {
+    try{
+      $stmt = $dbh->prepare('SELECT ELEMENT.idElement, ELEMENT.tasks, ELEMENT.deadLine, ELEMENT.done, ELEMENT.idUser, ELEMENT.idCategory FROM ELEMENT,CATEGORY WHERE idUser = ? AND CATEGORY.category = ? AND ELEMENT.idCategory = CATEGORY.idCategory');
+      $stmt->execute(array($_SESSION['currentUser'], $categor));
+      echo '
+            <table style="width:100%">
+                <tr>
+                  <th align="left">Task</th>
+                  <th align="left">Deadline</th>
+                  <th align="left">Done</th>
+                  </tr>';
+          while($row = $stmt->fetch()){
+            echo '
+                <tr>
+                  <td>'. $row['tasks'].'</td>
+                  <td>'. $row['deadLine'].'</td>';
+            if ($row['done'])
+              echo ' <td> <input type="checkbox" name="checkbox" disabled="disabled" checked> </td>';
+            else
+              echo ' <td> <input type="checkbox" name="checkbox" </td>';
+            echo' </tr>';
+            }
+      echo '</table>';
+    }
 
-    $row = $stmt->fetch();
-    echo 'Name: '. $row['name'];
+   catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
   }
-
- catch (Exception $e) {
-  echo 'Caught exception: ',  $e->getMessage(), "\n";
- }
 }
 
+function sortByDeadline($dbh) {
+    try{
+      $stmt = $dbh->prepare('SELECT * FROM ELEMENT WHERE idUser = ? Order by deadLine ASC');
+      $stmt->execute(array($_SESSION['currentUser']));
+      echo '
+            <table style="width:100%">
+                <tr>
+                  <th align="left">Task</th>
+                  <th align="left">Deadline</th>
+                  <th align="left">Done</th>
+                  </tr>';
+          while($row = $stmt->fetch()){
+            echo '
+                <tr>
+                  <td>'. $row['tasks'].'</td>
+                  <td>'. $row['deadLine'].'</td>';
+            if ($row['done'])
+              echo ' <td> <input type="checkbox" name="checkbox" disabled="disabled" checked> </td>';
+            else
+              echo ' <td> <input type="checkbox" name="checkbox" </td>';
+            echo' </tr>';
+            }
+      echo '</table>';
+    }
 
-
+   catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+  }
+}
 ?>
