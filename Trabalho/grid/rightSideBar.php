24e7b3e70829e5004fa7ef4895a824b29376ede3
eduardamsc <mariaeduardascunha97@gmail.com>
2017-12-08 20:38:34 +0000

    example photo and change in layout

--- Trabalho/grid/rightSideBar.php
+++ Trabalho/grid/rightSideBar.php
@@ -1,5 +1,5 @@
 <div id = "rightSideBar">
-
+<img id = "userPhoto" src="../assets/me.png" alt="User Photo" width=90%>
   <h4><b>Profile<b></h4>
     <?php getUserName($dbh); ?>
   <br>
@@ -7,9 +7,9 @@
   <br>
   <button id="logout">Logout</button>
   <div id = "listOptions">
-    <button id ="addLis">Add List </button>
+    <button id ="addLis">Add List</button>
     <br>
-    <button id ="remLis">Remove List </button>
+    <button id ="remLis">Remove List</button>
     <br>
   </div>
 
