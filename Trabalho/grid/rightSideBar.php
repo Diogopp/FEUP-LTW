54ad84ad07972d343a277366e69c51e4635bafc4
up201503447 <up201503447@fe.up.pt>
2017-12-12 21:33:38 +0000

    fixed image and gif size and buttons ajustment

--- Trabalho/grid/rightSideBar.php
+++ Trabalho/grid/rightSideBar.php
@@ -1,5 +1,5 @@
 <div id = "rightSideBar">
-  <img id = "userPhoto" src="../assets/me.png" alt="User Photo" width=90%>
+  <img id = "userPhoto" src="../assets/me.png" alt="User Photo" width=50%>
   <h4><b> <b></h4>
     <?php
     $user = getUserName($dbh);
@@ -9,12 +9,11 @@
   <button id="profile">Profile</button>
   <br>
   <button id="logout">Log out</button>
-  <div id = "listOptions">
-    <button id ="addLis">Add List</button>
-    <br>
-    <button id ="remLis">Remove List</button>
-    <br>
-  </div>
   <br>
-  <img id = "timeGif" src="../assets/time.gif" alt="quotes gif" width=90%>
+  <button id ="addLis">Add List</button>
+  <br>
+  <button id ="remLis">Remove List</button>
+  <br>
+  <br>
+  <img id = "timeGif" src="../assets/time.gif" alt="quotes gif" width=70%>
 </div>
